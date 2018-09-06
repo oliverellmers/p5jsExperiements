b295ee8e88b16ad20f858f8ecde7d5635b1654b2
Oliver Ellmers <hi@oliverellmers.com>
2018-09-06 14:45:43 +0100

    Cubes rotating working

--- js/rotateTest.js
+++ js/rotateTest.js
@@ -6,38 +6,72 @@ var easing = 0.05;
 
 var rotZFactor = 0;
 
+var p1, p2, dir;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  p1 = createVector(-200, 100, -200);
+  p2 = createVector(200, 100, -500);
 
 }
 
 function draw() {
   background(0);
 
+  //p1.x = map(mouseX, 0, width, -400, 400);
+  //p1.y = map(mouseY, 0, height, -400, 400);
+  p1.x = mouseX;
+  p1.y = mouseY;
 
   /*
-  angle = atan2(mouseX - height/2, mouseX - width/2);
-  var dir = (angle - targetAngle)/TWO_PI;
-  dir -= round(dir);
-  dir *= TWO_PI;
-
-  targetAngle += dir * easing;
+  dir = p5.Vector.sub(p2, p1);
+  var pitch = asin(dir.y / dir.mag());
+  var yaw = -asin(dir.x / (cos(pitch) * dir.mag()));
   */
 
-  var rotX = (mouseX/height) * -2 * PI + PI;
-  var rotY = (mouseX/width) * -2 * PI - PI;
-  var rotZ = rotZFactor * PI/36;
+  translate(width/2, height/2 + 100);
 
-  fill(255, 0, 0);
   push();
-  translate(0, 0, 0);
-  //rotate(targetAngle);
-  rotateX(rotX);  
-  rotateY(rotY); 
-  rotateZ(rotZ);  
-
-  box(70, 70, 70);
+  pointLight(250, 250, 250, p1.x, -p1.y, 400);
   pop();
+
+/*
+  push();
+  translate(-width/2 - 100,-height/2,0);
+  rotateX(-pitch);
+  rotateY(yaw);
+  //fill(0,255,0);
+  ambientMaterial(250);
+  box(100);
+  pop();
+*/
+
+  for(var x = 0; x < width; x+=200){
+    for(var y = 0; y < height; y+= 200){
+
+      var tempP2 = createVector(x, y, -400);
+      //dir = p5.Vector.sub(p2, p1);
+      dir = p5.Vector.sub(tempP2, p1);
+
+      var pitch = asin(dir.y / dir.mag());
+      var yaw = -asin(dir.x / (cos(pitch) * dir.mag()));
+
+
+      push();
+      translate(x - width,y - height, 0);
+      rotateX(-pitch);
+      rotateY(yaw);
+      ambientMaterial(250);
+      box(75);
+      pop();
+    }
+    
+
+  }
+
+
+  
+
 
 
 }
