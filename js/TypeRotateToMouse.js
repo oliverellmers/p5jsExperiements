0fbce2dfe24dd97c3df25449a858f84dd98009cd
Oliver Ellmers <oliver@random42.com>
2018-09-17 15:57:32 +0100

    dynamic resizing should be working now

--- js/TypeRotateToMouse.js
+++ js/TypeRotateToMouse.js
@@ -5,6 +5,8 @@ p5.disableFriendlyErrors = true;
 var p1, p2, dir;
 var isMousePressed = false;
 
+var multiCanvas;
+
 var boxBuffer;
 var textBuffersArr = [];
 
@@ -13,10 +15,15 @@ var fontsize = 32;
 
 var planeSize;
 var stepSize;
-var numGraphics = 0;
 
+var _w;
+var _h;
 
+var planeCount = 0;
 
+var isTextSetup = false;
+
+var textArr = [];
 
 function preload() {
   font = loadFont('assets/Agenda-Super210.otf');
@@ -24,129 +31,199 @@ function preload() {
 }
 
 function setup() {
-  var multiCanvas = createCanvas(windowWidth, windowHeight, P2D);//, WEBGL);
-  pixelDensity(1);
-  boxBuffer = createGraphics(windowWidth, windowHeight, WEBGL);
+  multiCanvas = createCanvas(windowWidth, windowHeight, P2D);
+  boxBuffer = createGraphics(windowWidth, windowHeight,WEBGL);
+  init();
 
-  
-  planeSize = width / 10;
+}
+
+function init(){
+  _w = windowWidth;
+  _h = windowHeight;
+  var _x = (windowWidth - width) / 2;
+  var _y = (windowHeight - height) / 2;
+
+  pixelDensity(1);
+
+  planeCount = 0;
+  planeSize = _w / 10;
   stepSize = planeSize * 1.1;
 
-  setupText();
-  setupBoxes();
-  
+  p1 = createVector(-200, 100, -200);
+  p2 = createVector(200, 100, -500);
 
+  setupText();
+  
+  multiCanvas.position(_x, _y);
   multiCanvas.parent("multiCanvas");
 }
 
 function draw() {
-  
-  drawText();
+  background(0);
+
+  _w = windowWidth;
+  _h = windowHeight;
+
   drawBoxes();
 
   if(isMousePressed){
-    filter('INVERT');
-  }
-  
-}
-
-function setupBoxes(){
-  p1 = createVector(-200, 100, -200);
-  p2 = createVector(200, 100, -500);
+    //filter('INVERT');
+  }  
 }
 
 function drawBoxes(){
   
   boxBuffer.background(0);
-  boxBuffer.size(windowWidth, windowHeight);
+  boxBuffer.size(_w, _h);
 
   p1.x = mouseX;
   p1.y = mouseY;
-  p1.z = -100;
+  p1.z = -300;
 
-  
-  var count = 0;
-  for(var x = 0; x < width - planeSize/2; x+=stepSize){
-    for(var y = 0; y < height - planeSize/2; y+= stepSize){
-      boxBuffer.resetMatrix();
-      boxBuffer.translate(width/2 + planeSize/2, height/2 + planeSize/2);
-      var tempP2 = createVector(x, y,1);
-      dir = p5.Vector.sub(tempP2, p1);
+  if(textBuffersArr.length == 0){
 
-      var pitch = asin(dir.y / dir.mag());
-      var yaw = -asin(dir.x / (cos(pitch) * dir.mag()));
+    setupText();
 
+    var n = 0;
 
-      push();
-      boxBuffer.translate(x - width,y - height, 0);
-      boxBuffer.rotateX(-pitch);
-      boxBuffer.rotateY(yaw);     
-      
-      boxBuffer.texture(textBuffersArr[count]);
-      boxBuffer.plane(planeSize, planeSize);
-      pop();
-      count++;
+    for(var x = 0; x < _w - planeSize/2; x+=stepSize){
+      for(var y = 0; y < _h - planeSize/2; y+= stepSize){
+
+        var letter = char(n + 35);
+
+        textBuffersArr[n].background(0);
+        textBuffersArr[n].fill('#ed225d');
+        textBuffersArr[n].textSize(fontsize);
+        textBuffersArr[n].textAlign(CENTER);
+        textBuffersArr[n].textStyle(BOLD);
+        textBuffersArr[n].text(letter, 32,32);
+        n++
+      }
     }
   }
+  
+  var numGraphics = 0;
+  var text = "NEW WORK COMING SOON";
+  textArr = (unchar(split(text, '')))
+  var textArrCount = 0;
 
-  image(boxBuffer, 0, 0, windowWidth, windowHeight);
+
+  if(isTextSetup){
+
+    for(var x = 0; x < _w - planeSize/2; x+=stepSize){
+      for(var y = 0; y < _h - planeSize/2; y+= stepSize){
+
+        boxBuffer.resetMatrix();
+        boxBuffer.translate(_w/2 + planeSize/2, _h/2 + planeSize/2);
+        var tempP2 = createVector(x, y,1);
+        dir = p5.Vector.sub(tempP2, p1);
+
+        var pitch = asin(dir.y / dir.mag());
+        var yaw = -asin(dir.x / (cos(pitch) * dir.mag()));
+
+        push();
+        boxBuffer.translate(x - _w,y - _h, 0);
+        boxBuffer.rotateX(-pitch);
+        boxBuffer.rotateY(yaw);
+        
+        if(planeCount < textBuffersArr.length){
+          var letter = char(planeCount + 35);
+          
+          //print(char(textArr[0]));
+
+          textBuffersArr[planeCount].background(0);
+
+          textBuffersArr[planeCount].fill('#ed225d');
+          textBuffersArr[planeCount].textSize(fontsize);
+          textBuffersArr[planeCount].textAlign(CENTER);
+          textBuffersArr[planeCount].textStyle(BOLD);
+          console.log();
+
+          if(planeCount % 4 == 0 && textArrCount < textArr.length){
+            textBuffersArr[planeCount].fill('#ffffff');
+            //textBuffersArr[planeCount].text(letter, 32,32);
+
+            textBuffersArr[planeCount].text(char(textArr[textArrCount]), 32,32);
+            
+            textArrCount++;
+
+
+          }else{
+            textBuffersArr[planeCount].fill('#3D3D3D');
+            textBuffersArr[planeCount].text(letter, 32,32);
+          }
+
+
+          //textBuffersArr[planeCount].text(letter, 32,32);
+
+          planeCount++;
+        }
+        
+        if(textBuffersArr[numGraphics] != null){
+          boxBuffer.texture(textBuffersArr[numGraphics]);
+        }
+        
+        boxBuffer.plane(planeSize, planeSize);
+        pop();
+        numGraphics++;
+      }
+    }
+    image(boxBuffer, 0, 0, _w, _h);
+  }
 }
 
 
 
 function setupText(){
-  
-  for(var x = 0; x < width - planeSize/2; x+=stepSize){
-    for(var y = 0; y < height - planeSize/2; y+= stepSize){
-        numGraphics += 1;
+  //console.log("setting up text");
+  var n = 0;
+
+  for(var x = 0; x < _w - planeSize/2; x+=stepSize){
+    for(var y = 0; y < _h - planeSize/2; y+= stepSize){
+      textBuffersArr[n] = createGraphics(64, 64, P2D);
+      n++;
     }
   }
 
-  for(var i = 0; i < numGraphics; i++){
-    textBuffersArr[i] = createGraphics(64, 64, P2D);
+  //console.log("number of created text graphics: " + textBuffersArr.length);
 
-  }
-}
-
-function drawText(){
-  
-
-  var counter = 35;
-
-  for(var i = 0; i < numGraphics; i++){
-    var letter = char(counter);
-
-    textBuffersArr[i].background(0);
-    textBuffersArr[i].fill('#ed225d');
-    textBuffersArr[i].textSize(fontsize);
-    textBuffersArr[i].textAlign(CENTER);
-    textBuffersArr[i].textStyle(BOLD);
-    textBuffersArr[i].text(letter, 32,32);
-
-    counter++;
-  }
-
+  isTextSetup = true;
 
 }
+
+function removeText(){
+
+  //console.log("removing all text graphics");
+  textBuffersArr = [];
+  //console.log("number of text graphics after removal: " + textBuffersArr.length);
+
+}
+
+
 
 function mousePressed(){
   isMousePressed = true;
 
 }
 
+function centerCanvas(){
+  var _x = (windowWidth - width) / 2;
+  var _y = (windowHeight - height) / 2;
+  multiCanvas.position(_x, _y);
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 
-  //SUPER hacky way of doing this - does not seem to work with the head buffer probably due to memory
-  multiCanvas.remove();
-  multiCanvas = createCanvas(windowWidth, windowHeight, P2D);
+  init();
+  removeText();
+  centerCanvas();
 
   boxBuffer.remove();
   boxBuffer = createGraphics(windowWidth, windowHeight, WEBGL);
-  //headBuffer.remove();
-  //headBuffer = createGraphics(windowWidth, windowHeight, WEBGL);
-
 }
+
+
 
 
 
